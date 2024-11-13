580dff506bf44fb07ffd27e461990e5ddefe79d5
ankitsvr <thakurankitsvr@gmail.com>
2024-11-13 16:08:28 +0530

    testing changes

--- writeups/views.py
+++ writeups/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,HttpResponseNotFound
 # Create your views here.
+
+
+   
+def blog(request,blog_page):
+    page_content = None
+    if blog_page == "ddos":
+        page_content="this will return DDOS blogs"
+    elif blog_page == "layer 7 attack":
+        page_content="This will return any general cybersecurity attack blogs"
+    else:
+        return HttpResponseNotFound("This page is not found") 
+    return HttpResponse(page_content)
